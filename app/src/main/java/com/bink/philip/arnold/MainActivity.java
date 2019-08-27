f0d9e2dc89375d7e7d04f41448289ea2382b024b
themrfill <philip.r.j.arnold@gmail.com>
2019-08-27 11:11:41 +0100

    first commit

---
+++ app/src/main/java/com/bink/philip/arnold/MainActivity.java
@@ -0,0 +1,21 @@
+package com.bink.philip.arnold;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bink.philip.arnold.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
